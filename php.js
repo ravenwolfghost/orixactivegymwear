4dd1f40309a06b0c75d31147a8f25d1ba711e63f
ravenwolfghost <festuskendrick@gmail.com>
2025-07-29 15:36:54 -0600

    Update php.js

--- php.js
+++ php.js
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["Submit Order"] == "POST") {
-    $to ="https://formsubmit.co/el/lahufi";
+    $to ="https://formsubmit.co/el/lahufi/festuskendrick3@gmail.com";
     $subject = "New Order Received";
     $orderDetails = print_r($_POST, true);
     $headers = "From: orders@yourwebsite.com";
