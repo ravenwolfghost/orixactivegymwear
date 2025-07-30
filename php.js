e127276eda396cc31da07dc8ee9552a4287c46c5
ravenwolfghost <festuskendrick@gmail.com>
2025-07-29 18:39:03 -0600

    Update php.js

--- php.js
+++ php.js
@@ -1,31 +1,10 @@
 <?php
-// processOrder.php
-
-// Check if POST data is received
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve the order data sent from the JavaScript
-    $orderId = isset($_POST['orderId']) ? $_POST['orderId'] : '';
-    $full name = isset($_POST['customerName']) ? $_POST['customerName'] : '';
-    $customerEmail = isset($_POST['customerEmail']) ? $_POST['customerEmail'] : '';
-    $customerEmail = isset($_POST['customerEmail']) ? $_POST['customerEmail'] : '';
+    $to = "festuskendrick3.com";
+    $subject = "New Order Received";
+    $orderDetails = print_r($_POST, true);
+    $headers = "https://6889684b751ed3af7d066c76--orixactivegymwear.netlify.app";
 
-    $orderTotal = isset($_POST['orderTotal']) ? $_POST['orderTotal'] : '';
-
-    // Process the order (for example, store it in the database or send an email)
-    // Here, we'll just simulate processing the order and send a confirmation response
-
-    // Example of what you might do:
-    // 1. Validate the data
-    // 2. Store the order in a database
-    // 3. Send an email notification to the admin/customer
-    // 4. Return a success message
-    
-    // Here we're simulating that the order has been placed successfully
-    $response = "Order #$orderId has been placed successfully. Total: $orderTotal";
-    
-    // Return the response to the JavaScript function
-    echo $response;
-} else {
-    echo "Invalid request method.";
+    mail($to, $subject, $orderDetails, $headers);
 }
 ?>
